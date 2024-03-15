d1b5a7158476e5c2a90b187313ada41c50e764bd
RACENI <saracen_dev@naver.com>
2024-03-16 01:14:54 +0900

    server 구현 완료

--- Screen Saver/TcpServer.cs
+++ Screen Saver/TcpServer.cs
@@ -15,48 +15,81 @@ namespace Screen_Saver
 {
     internal class TcpServer
     {
-        StreamReader streamReader1;
-        //StreamWriter streamWriter1;
-        Thread thread1;
-        TcpListener tcpListener1;
+        TcpListener tcpListener;
+        private bool isRunning = true;
 
-        ~TcpServer()
+        public void start()
         {
-            tcpListener1.Stop();
-            thread1.Abort();
-            MessageBox.Show("닫힘");
+            tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 2000);
+            tcpListener.Start();
+            //MessageBox.Show("서버 시작 완료");
+
+            // 클라이언트 연결을 수락하는 부분을 백그라운드 스레드로 이동
+            Thread acceptThread = new Thread(AcceptClients);
+            acceptThread.IsBackground = true;
+            acceptThread.Start();
         }
 
-        //서버 쓰레드 시작
-        public void ThreadStart()
+        private void AcceptClients()
         {
-            thread1 = new Thread(connect);
-            thread1.IsBackground = true; // 폼 종료시 쓰레드 종료
-            thread1.Start();
-        }
-
-
-/*        public void sendData(string text)
-        {
-            streamWriter1.WriteLine(text);
-        }*/
-
-        private void connect()
-        {
-            tcpListener1 = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080);
-            tcpListener1.Start();
-            MessageBox.Show("커넥트 함수 스타트 완료");
-            TcpClient tcpClient1 = tcpListener1.AcceptTcpClient();
-
-            streamReader1 = new StreamReader(tcpClient1.GetStream());
-
-            //streamWriter1 = new StreamWriter(tcpClient1.GetStream());
-            //streamWriter1.AutoFlush = true;
-
-            while(tcpClient1.Connected)
+            try
             {
-                string receiveData1 = streamReader1.ReadLine();
+                while (isRunning)
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    //MessageBox.Show("클라이언트 연결: " + ((IPEndPoint)client.Client.RemoteEndPoint).Address);
+
+                    // 각 클라이언트와의 통신을 담당하는 스레드 생성
+                    Thread clientThread = new Thread(HandleClient);
+                    clientThread.IsBackground = true;
+                    clientThread.Start(client);
+                }
             }
+            catch (Exception ex) { }
+        }
+
+        private void HandleClient(object clientObj)
+        {
+            TcpClient client = (TcpClient)clientObj;
+
+            using (StreamReader reader = new StreamReader(client.GetStream()))
+            using (StreamWriter writer = new StreamWriter(client.GetStream()) { AutoFlush = true })
+            {
+                try
+                {
+                    while (true)
+                    {
+                        string receiveData = reader.ReadLine();
+                        if (receiveData == null)
+                            break;
+
+                        switch (receiveData)
+                        {
+                            case "gd":
+                                writer.WriteLine("hi");
+                                break;
+                            default:
+                                writer.WriteLine("Unknown command");
+                                break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("클라이언트 연결 오류: " + ex.Message);
+                }
+                finally
+                {
+                    client.Close(); // 클라이언트 연결 닫기
+                }
+            }
+        }
+
+        // 서버 종료
+        public void Stop()
+        {
+            isRunning = false;
+            tcpListener.Stop();
         }
     }
 }
